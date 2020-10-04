c7ab8ecadc95a948266dcfe5740e0ace96dae445
MissCelia <1830170706@qq.com>
2020-10-04 13:57:13 +0800

    init project

---
+++ src/components/logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {}
+}
+
+</script>
+<style lang='stylus' rel='stylesheet/stylus'>
+</style>
